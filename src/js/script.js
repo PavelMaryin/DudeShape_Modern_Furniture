c754d38b773746fb569bafa5a5551251a20360e7
PavelMaryin <p.maryin@gmail.com>
2022-04-19 01:47:51 +0300

    added slider

--- src/js/script.js
+++ src/js/script.js
@@ -3,17 +3,29 @@ function testWebP(callback) {
 
   var webP = new Image();
   webP.onload = webP.onerror = function () {
-  callback(webP.height == 2);
+    callback(webP.height == 2);
   };
   webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-  }
-  
-  testWebP(function (support) {
-  
-  if (support == true) {
-  document.querySelector('body').classList.add('webp');
-  }else{
-  document.querySelector('body').classList.add('no-webp');
-  }
-  });
+}
 
+testWebP(function (support) {
+
+  if (support == true) {
+    document.querySelector('body').classList.add('webp');
+  } else {
+    document.querySelector('body').classList.add('no-webp');
+  }
+});
+
+@@include('../../node_modules/swiper/swiper-bundle.js');
+
+
+new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+
+  },
+  slidesPerView: 3,
+  spaceBetween: 30,
+});
